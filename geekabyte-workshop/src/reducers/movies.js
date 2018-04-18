48baf3ca74edafe72340bd46043d28bcef9a2557
Vihang Patel <vihang.engg@gmail.com>
2018-04-18 12:06:26 +0530

    byte-22 Implement reducer actions

--- geekabyte-workshop/src/reducers/movies.js
+++ geekabyte-workshop/src/reducers/movies.js
@@ -1,5 +1,18 @@
 const movies = (state = [], action) => {
-	return state
+	switch (action.type) {
+		case "STORE_NOW_SHOWING_MOVIES":
+			return {
+				...state,
+				nowShowing: action.payload,
+			}
+		case "STORE_TOP_RATED_MOVIES":
+			return {
+				...state,
+				topRated: action.payload,
+			}
+		default:
+			return state
+	}
 }
 
 export default movies
